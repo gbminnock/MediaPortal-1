02a4b5c8613b704a853e5cb0f72764f57bf2027a
sebastiii <sebastiii@team-mediaportal.com>
2012-10-11 13:11:48 +0200

    Add Files.

---
+++ TvEngine3/Mediaportal/TV/Server/TVDatabase/EntityModel/Model.Designer.cs
@@ -0,0 +1,4 @@
+﻿// Default code generation is disabled for model 'C:\Development\mediaportal-1-gibman\MediaPortal-1\TvEngine3\Mediaportal\TV\Server\TVDatabase\EntityModel\Model.edmx'. 
+// To enable default code generation, change the value of the 'Code Generation Strategy' designer
+// property to an alternate value. This property is available in the Properties Window when the model is
+// open in the designer.
